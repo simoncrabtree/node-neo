8e8e4dc4f8a87cf428bc46c92948793ea50dc50e
simoncrabtree <simon@crabsticks.net>
2014-05-10 19:16:27 +0100

    Use oboe to transform json result

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
-module.exports = function () {
-  return('node-neo');
+var request = require('request');
+var oboe = require('oboe');
+
+module.exports = function (cypher, params, callback) {
+  var results = [];
+  var requestQuery = request.post({
+    headers: {
+      'Accept': 'application/json',
+      'X-Stream': 'true'
+    },
+    url: 'http://localhost:7474/db/data/cypher',
+    json: {
+      query: cypher,
+      params: params
+    }
+  });
+
+  oboe(requestQuery)
+  .node('data[*]*', function (data) {
+    results.push(data);
+  })
+  .done(function (data) {
+    callback(null, results);
+  });
 };
